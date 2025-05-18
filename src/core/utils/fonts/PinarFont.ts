ea1ce96e696b410b74032976d79b602c1a968c98
fatemeh <babaei.f1996@gmail.com>
2025-05-18 12:00:23 +0330

    feat: add fonts

---
+++ src/core/utils/fonts/PinarFont.ts
@@ -0,0 +1,39 @@
+import localFont from "next/font/local";
+
+
+export const PinarFont = localFont({
+    src: [
+        {
+            path: '../../../fonts/pinar/woff2/Pinar-FD-Light.woff2',
+            weight: '300',
+            style: 'normal',
+        },
+        {
+            path: '../../../fonts/pinar/woff2/Pinar-FD-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../../../fonts/pinar/woff2/Pinar-FD-Medium.woff2',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: '../../../fonts/pinar/woff2/Pinar-FD-SemiBold.woff2',
+            weight: '600',
+            style: 'normal',
+        },
+        {
+            path: '../../../fonts/pinar/woff2/Pinar-FD-Bold.woff2',
+            weight: '800',
+            style: 'normal',
+        },
+        {
+            path: '../../../fonts/pinar/woff2/Pinar-FD-Black.woff2',
+            weight: '900',
+            style: 'normal',
+        },
+    ],
+    display: 'swap',
+    variable: '--font-pinar'
+});
