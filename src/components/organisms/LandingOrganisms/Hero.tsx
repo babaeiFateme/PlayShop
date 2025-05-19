a02f818eae33efc4cb018f7c4c867447822563ca
fatemeh <babaei.f1996@gmail.com>
2025-05-19 15:07:27 +0330

    feat: create approach component

--- src/components/organisms/LandingOrganisms/Hero.tsx
+++ src/components/organisms/LandingOrganisms/Hero.tsx
@@ -9,6 +9,26 @@ import Button from "@/components/atoms/Button/Button";
 const Hero = () => {
     const theme = useTheme();
 
+    const approach = [
+        {
+            id:1,
+            value: '200+',
+            label: 'International Brands'
+        },
+
+        {
+            id:2,
+            value: '2,000+',
+            label: 'High-Quality Products'
+        },
+
+        {
+            id:3,
+            value: '30,000+',
+            label: 'Happy Customers'
+        },
+    ]
+
     return (
         <div className="grid grid-cols-1 gap-2 lg:grid-cols-2 bg-[#F2F0F1] px-10 py-20">
             <div>
@@ -19,7 +39,7 @@ const Hero = () => {
                     garments, designed to bring out your individuality and cater
                     to your sense of style.
                 </p>
-                
+
                 <Button
                     variant="contained"
                     style={{ background: theme.palette.custom.black.main }}
@@ -38,6 +58,17 @@ const Hero = () => {
                     className="w-full"
                 />
             </div>
+
+            <ul className="flex gap-4 items-center flex-wrap mt-15">
+                {approach.map((item) => (
+                    <li key={item.id} className="flex flex-col gap-2 text-center border-l-1 border-gray-300 p-2 first:border-0">
+                        <span className="text-md lg:text-3xl font-semibold">
+                            {item.value}
+                        </span>
+                        <span className="text-gray-500">{item.label}</span>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
