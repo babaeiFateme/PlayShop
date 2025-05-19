568f50b3c4c41ceeba4cb2473201e58a7da6b979
fatemeh <babaei.f1996@gmail.com>
2025-05-19 14:58:33 +0330

    refactor: change style from Hero section

--- src/components/organisms/LandingOrganisms/Hero.tsx
+++ src/components/organisms/LandingOrganisms/Hero.tsx
@@ -10,11 +10,11 @@ const Hero = () => {
     const theme = useTheme();
 
     return (
-        <div className="grid grid-cols-1 gap-2 lg:grid-cols-2 bg-[#F2F0F1]">
+        <div className="grid grid-cols-1 gap-2 lg:grid-cols-2 bg-[#F2F0F1] px-10 py-20">
             <div>
-                <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+                <h1 className="font-bold lg:text-[60px] max-w-[600px]">FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
 
-                <p>
+                <p className="font-medium lg:text-lg text-gray-700 max-w-[600px] mb-10">
                     Browse through our diverse range of meticulously crafted
                     garments, designed to bring out your individuality and cater
                     to your sense of style.
@@ -23,7 +23,7 @@ const Hero = () => {
                 <Button
                     variant="contained"
                     style={{ background: theme.palette.custom.black.main }}
-                    className="rounded-lg"
+                    className="!rounded-xl !px-10 !py-2"
                 >
                     Shop Now
                 </Button>
