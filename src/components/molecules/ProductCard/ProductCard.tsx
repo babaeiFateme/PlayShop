6f07e1ea04886ee866edc84b013fa6da3ac0a50a
fatemeh <babaei.f1996@gmail.com>
2025-05-26 19:48:33 +0330

    feat: add product card

--- src/components/molecules/ProductCard/ProductCard.tsx
+++ src/components/molecules/ProductCard/ProductCard.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import Image from "next/image"
 
-const ProductCard = () => {
+const ProductCard = ({ src, title }) => {
   return (
-    <div>ProductCard</div>
+    <div className='rounded-md bg-white'>
+      <div className="bg-gray-100 rounded-lg">
+        <Image
+          src={src}
+          alt={title}
+          width={300}
+          height={200}
+          loading="lazy"
+          className="block mx-auto max-w-[300px] aspect-[1] object-cover"
+        />
+      </div>
+
+      <div className="flex flex-col gap-3 mt-3">
+        <h3>Lorem ipsum dolor sit.</h3>
+        <div className="font-semibold text-2xl">$120</div>
+      </div>
+    </div>
   )
 }
 
--- src/components/organisms/LandingOrganisms/TopSelling.tsx
+++ src/components/organisms/LandingOrganisms/TopSelling.tsx
@@ -1,14 +1,24 @@
+import Button from "@/components/atoms/Button/Button"
 import ProductCard from "@/components/molecules/ProductCard/ProductCard"
+
+import p1 from '@images/pages/products/p1.png'
 
 const TopSelling = () => {
 
     return (
         <div className=" mt-16">
-            <h2 className="text-center font-bold text-4xl">TOP SELLING</h2>
+            <h2 className="text-center font-bold text-4xl mb-10 mt-20">TOP SELLING</h2>
 
-            <div>
-                <ProductCard />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+                <ProductCard src={p1.src} title='loremdfbsmdbf sjkdfsdjf sjdfbsdjf'/>
+                <ProductCard src={p1.src} title='loremdfbsmdbf sjkdfsdjf sjdfbsdjf'/>
+                <ProductCard src={p1.src} title='loremdfbsmdbf sjkdfsdjf sjdfbsdjf'/>
+                <ProductCard src={p1.src} title='loremdfbsmdbf sjkdfsdjf sjdfbsdjf'/>
             </div>
+
+            <Button variant="outlined" color="secondary" className="!rounded-xl !block !mx-auto !my-20 !px-10 !py-2">
+                View All
+            </Button>
         </div>
     )
 }
