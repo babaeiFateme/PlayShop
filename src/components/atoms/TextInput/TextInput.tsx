1f5ee769cffe6026f30de5cf2862ba946f2ba374
fatemeh <babaei.f1996@gmail.com>
2025-05-27 19:26:59 +0330

    wip: refactor text input component

--- src/components/atoms/TextInput/TextInput.tsx
+++ src/components/atoms/TextInput/TextInput.tsx
@@ -4,14 +4,18 @@ import React from 'react'
 interface ITextInput {
     className?: string
     placeholder?: string
+    name: string,
+    type?: string
 }
 
-const TextInput = ({ className, ...props }: ITextInput) => {
+const TextInput = ({ className, type = 'text', name = '', ...props }: ITextInput) => {
     return (
         <TextField
+            type={type}
             hiddenLabel
             variant="filled"
             className={`rounded-md w-full !min-h-[15px] ${className}`}
+            name={name}
             {...props}
         />
     )
