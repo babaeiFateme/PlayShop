2f2c4703ec6899f1a6c8b9a5ede5f26b0db62502
fatemeh <babaei.f1996@gmail.com>
2025-05-29 10:20:21 +0330

    feat: update next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images:{
-    domains:['placehold.co', 'i.imgur.com','i.pinimg.com','placeimg.com','pravatar.cc']
+    domains:['placehold.co', 'i.imgur.com','i.pinimg.com','placeimg.com','pravatar.cc','example.com']
   }
 };
 
