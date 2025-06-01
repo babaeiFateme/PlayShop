ec8b71feefd4dc0815b5fa158ebbb380cdc3833e
fatemeh <babaei.f1996@gmail.com>
2025-06-01 10:09:06 +0330

    chore: change mobile style grid

--- src/template/Products.tsx
+++ src/template/Products.tsx
@@ -25,7 +25,7 @@ const Products = () => {
     if (!data || !Array.isArray(data)) return <p>داده‌ای برای نمایش وجود ندارد.</p>
 
     return (
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 px-2 lg:px-[40px] gap-8 md:max-w-[900px]  lg:max-w-[1200px] mx-auto'>
+        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 px-2 lg:px-[40px] gap-8 md:max-w-[900px]  lg:max-w-[1200px] mx-auto'>
             {data.map((product: IProduct) =>
                 !hydrated || isFetching ? (
                     <CardSkeleton key={product.id} />
