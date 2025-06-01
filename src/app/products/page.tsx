181dc1aa2fde75f2b0903d49b0e3a61efe3cc008
fatemeh <babaei.f1996@gmail.com>
2025-06-01 10:43:14 +0330

    refactor: update fetching in products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,25 +1,12 @@
-import { dehydrate, QueryClient } from '@tanstack/react-query'
 import React from 'react'
 import Products from '@/template/Products'
-import ReactQueryProvider from '@/lib/react-query-provider'
 import fetchHandler from '@/core/helpers/fetchHandler'
 import API_ENDPOINTS from '@/core/constants/api-endpoints/api-endpoints.constants'
 
 const Page = async () => {
-    const queryClient = new QueryClient()
+    const fetchResponse = await fetchHandler(API_ENDPOINTS.products)
 
-    await queryClient.ensureQueryData({
-        queryKey: ['products'],
-        queryFn: () => fetchHandler(API_ENDPOINTS.products)
-    })
-
-    const dehydratedState = dehydrate(queryClient)
-
-    return (
-        <ReactQueryProvider state={dehydratedState}>
-            <Products />
-        </ReactQueryProvider>
-    )
+    return <Products initialData={fetchResponse} />
 }
 
 export default Page
--- src/template/Products.tsx
+++ src/template/Products.tsx
@@ -6,19 +6,13 @@ import API_ENDPOINTS from '@/core/constants/api-endpoints/api-endpoints.constant
 import fetchHandler from '@/core/helpers/fetchHandler'
 import IProduct from '@/core/types/product.types'
 import { useQuery } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
 
-const Products = () => {
+const Products = ({ initialData }: { initialData: IProduct[] }) => {
     const { data, error, isFetching } = useQuery({
         queryKey: ['products'],
-        queryFn: () => fetchHandler(API_ENDPOINTS.products)
+        queryFn: () => fetchHandler(API_ENDPOINTS.products),
+        initialData: initialData
     })
-
-    const [hydrated, setHydrated] = useState(false)
-
-    useEffect(() => {
-        setHydrated(true)
-    }, [])
 
     if (error) return <p>خطا در دریافت اطلاعات!</p>
 
@@ -27,11 +21,7 @@ const Products = () => {
     return (
         <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 px-2 lg:px-[40px] gap-8 md:max-w-[900px]  lg:max-w-[1200px] mx-auto'>
             {data.map((product: IProduct) =>
-                !hydrated || isFetching ? (
-                    <CardSkeleton key={product.id} />
-                ) : (
-                    <ProductCard key={product.id} product={product} />
-                )
+                isFetching ? <CardSkeleton key={product.id} /> : <ProductCard key={product.id} product={product} />
             )}
         </div>
     )
