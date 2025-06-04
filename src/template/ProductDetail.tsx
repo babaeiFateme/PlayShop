f830b0cd3efc132aa1ba236efe0644432c014e2f
fatemeh <babaei.f1996@gmail.com>
2025-06-04 11:48:39 +0330

    refactor: control change product

--- src/template/ProductDetail.tsx
+++ src/template/ProductDetail.tsx
@@ -1,21 +1,21 @@
 'use client'
 
 import Button from '@/components/atoms/Button/Button'
-import CardSkeleton from '@/components/atoms/Skeleton/CardSkeleton'
+// import CardSkeleton from '@/components/atoms/Skeleton/CardSkeleton'
 import TextInput from '@/components/atoms/TextInput/TextInput'
 import ProductCard from '@/components/molecules/ProductCard/ProductCard'
-import fetchHandler from '@/core/helpers/fetchHandler'
+// import fetchHandler from '@/core/helpers/fetchHandler'
 import IProduct from '@/core/types/product.types'
 import { useTheme } from '@mui/material'
-import { useQuery } from '@tanstack/react-query'
+// import { useQuery } from '@tanstack/react-query'
 import Image from 'next/image'
 import React from 'react'
 
 const ProductDetail = ({
     product,
     related,
-    productEndPoint,
-    relatedEndPoint
+    // productEndPoint,
+    // relatedEndPoint
 }: {
     product: IProduct
     related: IProduct[]
@@ -24,17 +24,17 @@ const ProductDetail = ({
 }) => {
     const theme = useTheme()
 
-    const { data: productData = product } = useQuery({
-        queryKey: ['product'],
-        queryFn: () => fetchHandler(productEndPoint),
-        initialData: product
-    })
+    // const { data: productData = product } = useQuery({
+    //     queryKey: ['product'],
+    //     queryFn: () => fetchHandler(productEndPoint),
+    //     initialData: product
+    // })
 
-    const { data: relatedData = related, isFetching: isFetchingRelated } = useQuery({
-        queryKey: ['related-products'],
-        queryFn: () => fetchHandler(relatedEndPoint),
-        initialData: related
-    })
+    // const { data: relatedData = related, isFetching: isFetchingRelated } = useQuery({
+    //     queryKey: ['related-products'],
+    //     queryFn: () => fetchHandler(relatedEndPoint),
+    //     initialData: related
+    // })
 
     return (
         <div className='px-5 md:px-20 py-10'>
@@ -43,24 +43,24 @@ const ProductDetail = ({
                     <Image
                         width={400}
                         height={400}
-                        src={productData.images?.[0] || '/placeholder.jpg'}
-                        alt={productData.title}
+                        src={product.images?.[0] || '/placeholder.jpg'}
+                        alt={product.title}
                         className='w-full aspect-[1.2] max-w-[700px] rounded-md object-cover'
                         loading='lazy'
                     />
                 </div>
 
                 <div className='relative'>
-                    <h1 className='mb-5 lg:text-4xl font-bold grow'>{productData?.title}</h1>
+                    <h1 className='mb-5 lg:text-4xl font-bold grow'>{product?.title}</h1>
 
                     <div className='my-2'>
                         <span className='text-lg font-semibold'>Price: </span>
-                        <span className='text-2xl !text-primary'>{productData?.price} $</span>
+                        <span className='text-2xl !text-primary'>{product?.price} $</span>
                     </div>
 
                     <div className='my-2'>
                         <span className='text-lg font-semibold'>Category: </span>
-                        <span className='!text-primary'>{productData?.category?.name}</span>
+                        <span className='!text-primary'>{product?.category?.name}</span>
                     </div>
 
                     <hr className='my-10' />
@@ -101,7 +101,7 @@ const ProductDetail = ({
                         </div>
                     </div>
 
-                    <div className='mt-5 text-lg text-justify'>{productData?.description}</div>
+                    <div className='mt-5 text-lg text-justify'>{product?.description}</div>
                 </div>
             </div>
 
@@ -110,12 +110,12 @@ const ProductDetail = ({
             <div>
                 <h2 className='uppercase text-lg lg:text-4xl font-semibold text-center mb-10'>You might also like</h2>
                 <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 px-2 lg:px-[40px] gap-8 md:max-w-[900px] lg:max-w-[1200px] mx-auto'>
-                    {relatedData.map((product: IProduct) =>
-                        isFetchingRelated ? (
-                            <CardSkeleton key={product.id} />
-                        ) : (
+                    {related.map((product: IProduct) =>
+                        // isFetchingRelated ? (
+                        //     <CardSkeleton key={product.id} />
+                        // ) : (
                             <ProductCard key={product.id} product={product} />
-                        )
+                        // )
                     )}
                 </div>
             </div>
