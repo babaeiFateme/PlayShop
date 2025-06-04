7cf942553abd50e4aa428a739b4ba819c23a8bd5
fatemeh <babaei.f1996@gmail.com>
2025-06-04 17:11:38 +0330

    feat: add search for products

--- src/components/atoms/TextInput/ITextInputProps.ts
+++ src/components/atoms/TextInput/ITextInputProps.ts
@@ -3,7 +3,7 @@ import { TextFieldProps, TextFieldVariants } from '@mui/material'
 /*TODO: type to generic for filled */
 interface ITextInputProps extends Omit<TextFieldProps<'filled'>, 'variant'> {
     className?: string
-    variant?: TextFieldVariants
+    variant?: TextFieldVariants,
 }
 
 export default ITextInputProps
